3d27b6ad1e73502bb13bd1425442f2a86f783a28
olahimself <ambali@wiseki.com>
2024-07-07 16:23:49 +0100

    new fix

--- lib/src/core/constants/network_constant.dart
+++ lib/src/core/constants/network_constant.dart
@@ -2,5 +2,5 @@ String baseUrl = 'https://api.timbu.cloud';
 String apiKey = '0c5d3544d006454d98c3f80b3fd8320120240704224821266850';
 String appID = 'GZINFHHN4PLX81Z';
 String token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMDIyNzkxYjY2ZmRlNDA1ZTkzN2QzYjY3OWFkNDQ2MDkiLCJleHAiOjE3MjAzNTY3MDN9.Npv_8VuxIX_RIWyKqxoZI9O4_3paAxJnEw1hIte6zu8';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMDIyNzkxYjY2ZmRlNDA1ZTkzN2QzYjY3OWFkNDQ2MDkiLCJleHAiOjE3MjAzNjYyNzd9.oHn2pjrl8Xf-_Jkkb607nmNWggt4uNGS5uLsWi0_hIg';
 String organizationID = 'ecfa04b97a36489980150a75f9a23d3b';
